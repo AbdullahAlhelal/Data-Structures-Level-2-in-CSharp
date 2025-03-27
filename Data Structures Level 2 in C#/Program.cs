9b527fee57278bafa5b4b7efeb18f0c7ca1330ca
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-27 22:53:51 +0300

    first Commit

---
+++ Data Structures Level 2 in C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_Level_2_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
