f6d6c268f58de20b564061988969b353ae7dd7fe
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-29 21:50:20 +0300

    Sorting a List<int>

--- ListExamples/Program.cs
+++ ListExamples/Program.cs
@@ -137,6 +137,32 @@ namespace ListExamples
             Console.ReadKey();
 
         }
+      
+        
+        static void Sorting()
+        {
+            // List initialization
+            List<int> numbers = new List<int> { 44 , 22 , 55 , 666 , 9 , -6 , 345 , 11 , 3 , 3 };
+
+      
+            // Default Sorting (Ascending)
+            numbers.Sort(,);
+            Console.WriteLine("Sorted in Ascending Order: " + string.Join(", " , numbers));
+
+
+            // Sorting in Descending Order
+            numbers.Reverse();
+            Console.WriteLine("Sorted in Descending Order: " + string.Join(", " , numbers));
+
+
+            // Sorting with LINQ
+            Console.WriteLine("Sorted Ascending with LINQ: " + string.Join(", " , numbers.OrderBy(n => n)));
+            Console.WriteLine("Sorted Descending with LINQ: " + string.Join(", " , numbers.OrderByDescending(n => n)));
+
+
+            // Waiting for a key press
+            Console.ReadKey();
+        }
         static void Example1()
         {
             // Creating a new List of integers
