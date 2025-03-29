ae18d986e299cdeac200d40fbac8fb73b6d60b4c
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-29 22:05:58 +0300

    Exploring Contains, Exists, Find, FindAll, and Any with List<int>

--- ListExamples/Program.cs
+++ ListExamples/Program.cs
@@ -139,6 +139,36 @@ namespace ListExamples
         }
       
         
+        static void Exploring_Contains_Exists_Find_FindAll_and_Any_with_List()
+        {
+
+            // List initialization
+            List<int> numbers = new List<int> { 44 , 22 , 55 , 666 , 9 , -6 , 345 , 11 , 3 , 3 };
+
+
+            // Using Contains
+            Console.WriteLine("List contains 9: " + numbers.Contains(9));
+
+
+            // Using Exists
+            Console.WriteLine("List contains negative numbers: " + numbers.Exists(n => n < 0));
+
+
+            // Using Find
+            Console.WriteLine("First negative number: " + numbers.Find(n => n < 0));
+
+
+            // Using FindAll
+            Console.WriteLine("All negative numbers: " + string.Join(", " , numbers.FindAll(n => n < 0)));
+
+
+            // Using Any
+            Console.WriteLine("Any numbers greater than 100: " + numbers.Any(n => n > 100));
+
+
+            // Waiting for a key press
+            Console.ReadKey();
+        }
         static void Sorting()
         {
             // List initialization
