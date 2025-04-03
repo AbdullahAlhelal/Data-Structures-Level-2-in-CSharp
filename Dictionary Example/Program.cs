adaf2ec05fb5ef0b98faae718b157debf184257d
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-03 22:49:28 +0300

    UtilizingLINQwithDictionaries

--- Dictionary Example/Program.cs
+++ Dictionary Example/Program.cs
@@ -8,6 +8,76 @@ namespace Dictionary_Example
 {
     internal class Program
     {
+
+        static void UtilizingLINQwithDictionaries()
+        {
+            // Creating and initializing the dictionary
+            Dictionary<string , int> fruitBasket = new Dictionary<string , int>
+        {
+            { "Apple", 5 },
+            { "Banana", 2 },
+            { "Orange", 7 }
+        };
+
+
+            // Using LINQ to transform items
+            var fruitInfo = fruitBasket.Select(kpv => new { kpv.Key , kpv.Value });
+
+
+            // Displaying the results
+            Console.WriteLine("Transformed Items:");
+            foreach ( var item in fruitInfo )
+            {
+                Console.WriteLine($"Fruit: {item.Key}, Quantity: {item.Value}");
+            }
+
+
+
+
+            // Using LINQ to filter items
+            var filteredFruit = fruitBasket.Where(kpv => kpv.Value > 3);
+
+
+            Console.WriteLine("\nFiltered Items >3:");
+            foreach ( var item in filteredFruit )
+            {
+                Console.WriteLine($"Fruit: {item.Key}, Quantity: {item.Value}");
+            }
+
+
+
+
+            // Using LINQ to sort by value
+            var sortedByQuantity = fruitBasket.OrderBy(kpv => kpv.Value);
+
+
+            Console.WriteLine("\nSorted Items:");
+            foreach ( var item in sortedByQuantity )
+            {
+                Console.WriteLine($"Fruit: {item.Key}, Quantity: {item.Value}");
+            }
+
+
+            // Using LINQ to sort by value
+            var sortedByQuantityDesc = fruitBasket.OrderByDescending(kpv => kpv.Value);
+
+
+            Console.WriteLine("\nSorted Items Desc:");
+            foreach ( var item in sortedByQuantityDesc )
+            {
+                Console.WriteLine($"Fruit: {item.Key}, Quantity: {item.Value}");
+            }
+
+            // Using LINQ to aggregate data
+            int totalQuantity = fruitBasket.Sum(kpv => kpv.Value);
+
+            Console.WriteLine($"\nTotal Quantity: {totalQuantity}");
+
+
+            Console.ReadKey();
+        }
+
+
         static void Main(string[] args)
         {
             Dictionary<string , int> fruitBasket = new Dictionary<string , int>();
@@ -30,7 +100,7 @@ namespace Dictionary_Example
 
             foreach ( KeyValuePair<string , int> item in fruitBasket )
             {
-              
+
                 Console.WriteLine($"Fruit: {item.Key}, Quantity: {item.Value}");
             }
 
@@ -46,3 +116,4 @@ namespace Dictionary_Example
         }
     }
 }
+
