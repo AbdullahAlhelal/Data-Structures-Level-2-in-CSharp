a9fbeeceb9590ad45e2e08377007ad95e735d3c7
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-04 20:50:25 +0300

    Using SetEquals with HashSet

--- Hashset/Program.cs
+++ Hashset/Program.cs
@@ -82,6 +82,17 @@ namespace Hashset
         }
         #endregion
 
+        static void UsingSetEqualswithHashSet()
+        {
+            HashSet<int> set1 = new HashSet<int> { 1 , 2 , 3 };
+            HashSet<int> set2 = new HashSet<int> { 1 , 2 , 3 };
+            HashSet<int> set3 = new HashSet<int> { 3 , 4 , 5 };
+
+
+            Console.WriteLine("set1 equals set2: " + set1.SetEquals(set2));
+            Console.WriteLine("set1 equals set3: " + set1.SetEquals(set3));
+            Console.ReadKey();
+        }
         static void UsingHashSetwithLINQ()
         {
             // Creating and populating a HashSet of integers
@@ -207,6 +218,7 @@ namespace Hashset
             IntersectionOperationwithHashSet();
             DifferenceOperationwithHashSet();
             SymmetricDifferenceOperationwithHashSet();
+            UsingSetEqualswithHashSet();
         }
     }
 }
