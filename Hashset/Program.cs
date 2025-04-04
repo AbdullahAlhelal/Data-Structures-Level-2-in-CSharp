2b66aaaa08c27cac4872c3ba1ef3f4838d2aae3e
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-04 19:58:57 +0300

    introdaction to  HashSet Checking for Existence in HashSet

---
+++ Hashset/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashset
+{
+    internal class Program
+    {
+
+        static void CheckingforExistenceinHashSet()
+        {
+
+            // Creating and populating a HashSet
+            HashSet<string> fruits = new HashSet<string> { "Apple" , "Banana" , "Cherry" };
+
+
+            // Checking if "Apple" is in the HashSet
+            if ( fruits.Contains("Apple") )
+            {
+                Console.WriteLine("Apple is in the HashSet");
+            }
+            else
+            {
+                Console.WriteLine("Apple is not in the HashSet");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Creating a HashSet of strings
+            HashSet<string> fruits = new HashSet<string>();
+
+
+
+
+            // Adding elements to the HashSet
+            fruits.Add("Apple");
+            fruits.Add("Banana");
+            fruits.Add("Cherry");
+
+
+            // Trying to add a duplicate element
+            fruits.Add("Apple"); // This will not be added
+            fruits.Add("Apple"); // This will not be added
+            fruits.Add("Apple"); // This will not be added
+
+
+            // Displaying the elements in the HashSet
+            foreach ( string fruit in fruits )
+            {
+                Console.WriteLine(fruit);
+            }
+            Console.ReadKey();
+        }
+    }
+}
