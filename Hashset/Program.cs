5f2f1c46624fc09a84af754151899d66178d18b3
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-04 20:41:05 +0300

    IntersectionOperationwithHashSet impl DifferenceOperationwithHashSet

--- Hashset/Program.cs
+++ Hashset/Program.cs
@@ -10,9 +10,41 @@ namespace Hashset
     internal class Program
     {
         #region OperationwithHashSet
+        static void DifferenceOperationwithHashSet()
+        {
+            HashSet<int> set1 = new HashSet<int> { 1 , 2 , 3 };
+            HashSet<int> set2 = new HashSet<int> { 3 , 4 , 5 };
+
+
+            // Removing elements of set2 from set1
+            set1.ExceptWith(set2);
+
+
+            Console.WriteLine("Difference of sets (set1 - set2):");
+            foreach ( int item in set1 )
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
         static void IntersectionOperationwithHashSet()
         {
+            HashSet<int> set1 = new HashSet<int> { 1 , 2 , 3 };
+            HashSet<int> set2 = new HashSet<int> { 3 , 4 , 5 };
+
+
+            // Intersection of set1 and set2
+            set1.IntersectWith(set2);
+
+
+            Console.WriteLine("Intersection of sets:");
+            foreach ( int item in set1 )
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
+
         static void UnionOperationwithHashSetin()
         {
             HashSet<int> set1 = new HashSet<int> { 1 , 2 , 3 };
@@ -154,6 +186,8 @@ namespace Hashset
             CheckingforExistenceinHashSet();
             RemovingElementsfromHashSet();
             UnionOperationwithHashSetin();
+            IntersectionOperationwithHashSet();
+            DifferenceOperationwithHashSet();
         }
     }
 }
