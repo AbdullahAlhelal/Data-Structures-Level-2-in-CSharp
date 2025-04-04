46951b5cbbe23156432c9b787d1bf7d60e80bb0f
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-04 20:02:55 +0300

    RemovingElementsfromHashSet

--- Hashset/Program.cs
+++ Hashset/Program.cs
@@ -3,12 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Hashset
 {
     internal class Program
     {
 
+        static void RemovingElementsfromHashSet()
+        {
+
+            // Creating and populating a HashSet
+            HashSet<string> fruits = new HashSet<string> { "Apple" , "Banana" , "Cherry" };
+
+            Console.WriteLine("Hashset Item Count = " + fruits.Count.ToString());
+
+            // Removing "Banana" from the HashSet
+            fruits.Remove("Banana");
+
+            Console.WriteLine("\nHashset Item Count after removing Banana = " + fruits.Count.ToString());
+            // Displaying the remaining elements
+            foreach ( string fruit in fruits )
+            {
+                Console.WriteLine(fruit);
+            }
+
+            //this will remove all elements
+            fruits.Clear();
+            Console.WriteLine("\nHashset Item Count after clear = " + fruits.Count.ToString());
+
+            Console.ReadKey();
+        }
         static void CheckingforExistenceinHashSet()
         {
 
@@ -54,6 +79,9 @@ namespace Hashset
                 Console.WriteLine(fruit);
             }
             Console.ReadKey();
+
+            CheckingforExistenceinHashSet();
+            RemovingElementsfromHashSet();
         }
     }
 }
