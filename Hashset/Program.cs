ece1987fa9d7f5a2975cbf46faaa491a154fd7ab
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-04 21:09:07 +0300

    UsingIsSupersetOfwithHashSetin UsingOverlapswithHashSet

--- Hashset/Program.cs
+++ Hashset/Program.cs
@@ -95,6 +95,26 @@ namespace Hashset
             Console.ReadKey();
         }
 
+        static void UsingOverlapswithHashSet()
+        {
+            HashSet<int> set1 = new HashSet<int> { 1 , 2 , 3 };
+            HashSet<int> set2 = new HashSet<int> { 3 , 4 , 5 };
+            HashSet<int> set3 = new HashSet<int> { 6 , 7 , 8 };
+
+
+            Console.WriteLine("set1 overlaps set2: " + set1.Overlaps(set2));
+            Console.WriteLine("set1 overlaps set3: " + set1.Overlaps(set3));
+            Console.ReadKey();
+        }
+        static void UsingIsSupersetOfwithHashSetin()
+        {
+            HashSet<int> set1 = new HashSet<int> { 1 , 2 , 3 , 4 , 5 };
+            HashSet<int> set2 = new HashSet<int> { 2 , 3 };
+
+
+            Console.WriteLine("set1 is a superset of set2: " + set1.IsSupersetOf(set2));
+            Console.ReadKey();
+        }
         static void UsingIsSubsetOfwithHashSet()
         {
             HashSet<int> set1 = new HashSet<int> { 1 , 2 };
@@ -225,6 +245,8 @@ namespace Hashset
             }
             Console.ReadKey();
 
+            
+
             UsingHashSetwithLINQ();
             CheckingforExistenceinHashSet();
             RemovingElementsfromHashSet();
@@ -234,6 +256,8 @@ namespace Hashset
             SymmetricDifferenceOperationwithHashSet();
             UsingSetEqualswithHashSet();
             UsingIsSubsetOfwithHashSet();
+            UsingIsSupersetOfwithHashSetin();
+            UsingOverlapswithHashSet();
         }
     }
 }
